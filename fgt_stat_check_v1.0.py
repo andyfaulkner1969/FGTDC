b52a0e0f57f359d31e7fba7d6e8bb0b037b20b8b
andyfaulkner1969 <andyfaulkner1969@gmail.com>
2023-02-15 17:05:26 -0500

    Added dir add if missing, dir size limit

--- fgt_stat_check_v1.0.py
+++ fgt_stat_check_v1.0.py
@@ -2,7 +2,6 @@
 
 import paramiko
 import time
-import sys
 import time
 import os
 import shutil
@@ -14,9 +13,14 @@ import yaml
 #  This list can ge changed as needed.
 #  Configuration is done in the fgt_stat_check_config.yml file for your specfic Fortigate.
 #  It will log into the FGT via SSH and run each command logging to file.
-#  Create a sub dir call "logs"
-#  Created by Andy Faulkner afaulkner@fortinet.com a.k.a The Evil Bastard
-
+#  If logs directory is missing it will be created
+""""
+ ________         ____     _ __  ___           __              __
+/_  __/ /  ___   / __/  __(_) / / _ )___ ____ / /____ ________/ /
+ / / / _ \/ -_) / _/| |/ / / / / _  / _ `(_-</ __/ _ `/ __/ _  /
+/_/ /_//_/\__/ /___/|___/_/_/ /____/\_,_/___/\__/\_,_/_/  \_,_/
+afaulkner@fortinet.com 
+"""
 
 now = datetime.now()
 current_time = now.strftime("Date-%m-%d-%Y---Time-%H%M%S")
@@ -29,8 +33,13 @@ with open('fgt_stat_check_config.yml', 'r') as file:
 # Opening command list file
 cmd_file = open('fgt_commands.txt', 'r')
 
-# Opening log file
-fout = open(config['script_file_cfg']['log_path'] + config['script_file_cfg']['log_file'],'a')
+# Checking to see if log directory is there.
+def check_log_dir():
+	if os.path.isdir(config['script_file_cfg']['log_path']):
+		pass
+	else:
+		print("Missing logs directory...creating it now.")
+		os.makedirs(config['script_file_cfg']['log_path'])
 
 # Checking to see if log file exsist, if not creating.
 def check_file():
@@ -38,20 +47,25 @@ def check_file():
 		pass
 	else:
 		fout = open(config['script_file_cfg']['log_path'] + config['script_file_cfg']['log_file'],'wb')
+		print("1st time run creating log file.")
 		fout.close()
 
-# in progress directory limit check
+# Checking directory size, this is a safe guard to prevent the script from filling
+# the file system.
 def dir_size_limit():
-	for root, dirs, files in os.walk("/Users/andyfaulkner/Downloads/"):
+	for root, dirs, files in os.walk(config['script_file_cfg']['log_path']):
 		dir_size = 0
 		for fn in files:
 			path = os.path.join(root, fn)
 			size = os.stat(path).st_size # in bytes
 			#print(size)
 			dir_size = dir_size + size
+	if dir_size > int(config['script_file_cfg']['dir_limit']):
+		print("Directory Limit Reached!  Clean out directory and start again.")
+		exit()
+	else:
+		print("Directory size good... moving to next step")
 			
-	print(dir_size)
-
 # Checking to see if log file has got too large, if yes move it with date stamp.
 		# File limit set in yaml file.
 def log_file_rotation():
@@ -83,7 +97,8 @@ def console_set():
 
 # Running commands against FGT taken from commands file.
 def run_command():
-
+	# Opening log file
+	fout = open(config['script_file_cfg']['log_path'] + config['script_file_cfg']['log_file'],'a')
 	host = config['fgtconfig']['fgt_ip']
 	port = config['fgtconfig']['fgt_port']
 	username = config['fgtconfig']['fgt_admin']
@@ -91,7 +106,6 @@ def run_command():
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(host, port, username, password)	
-
 	fout.write(current_time)
 	fout.write('\n\r')
 	fout.write("***** Start of commands *****")
@@ -118,7 +132,9 @@ def run_command():
 	cmd_file.close()
 	ssh.close()
 
-log_file_rotation()
+check_log_dir()
+dir_size_limit()
 check_file()
+log_file_rotation()
 console_set()
 run_command()
